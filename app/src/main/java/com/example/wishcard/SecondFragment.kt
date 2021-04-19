0dee5efbc79b070d040386339207938de4170165
dtzova <80419544+dtzova@users.noreply.github.com>
2021-04-19 15:01:22 +0300

    Added UI

---
+++ app/src/main/java/com/example/wishcard/SecondFragment.kt
@@ -0,0 +1,14 @@
+package com.example.wishcard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+    }
